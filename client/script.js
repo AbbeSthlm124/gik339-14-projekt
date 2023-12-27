05a5748898394a6acd7431d2d8ebd6faeee98c0c
Abukar Mohamed <120708360+AbbeSthlm124@users.noreply.github.com>
2023-12-27 20:09:44 +0100

    changes in file script and index

--- client/script.js
+++ client/script.js
@@ -2,3 +2,43 @@ const url = "http://localhost:3000/bilr";
 fetch(url)
   .then((result) => result.json())
   .then((bilr) => console.log(bilr));
+
+const manufacturers = [
+  "Aston Martin",
+  "Audi",
+  "Bentley",
+  "BMW",
+  "Bugatti",
+  "Ford",
+  "Honda",
+  "Jaguar",
+  "Jeep",
+  "Lamborghini",
+  "Land Rover",
+  "Maybach",
+  "McLaren",
+  "Mercedes-Benz",
+  "Mitsubishi",
+  "Nissan",
+  "Opel",
+  "Peugeot",
+  "Porsche",
+  "Rolls-Royce",
+  "Saab",
+  "Subaru",
+  "Suzuki",
+  "Tesla",
+  "Toyota",
+  "Volkswagen",
+  "Volvo",
+  "W Motors",
+];
+
+const selectManufacturers = document.getElementById("mfr");
+
+manufacturers.forEach((manufacturer) => {
+  const option = document.createElement("option");
+  option.value = manufacturer.toUpperCase();
+  option.text = manufacturer;
+  selectManufacturers.appendChild(option);
+});
